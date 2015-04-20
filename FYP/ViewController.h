d589899b79d74fac102864bb542a4bb91c4f9d8b
Samuel Seah <Samuel@Samuels-Mac-2.gateway.2wire.net>
2015-04-21 00:19:35 +0800

    Initial Commit

---
+++ FYP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FYP
+//
+//  Created by Samuel Seah on 21/4/15.
+//  Copyright (c) 2015 Samuel Seah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FYP/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  FYP
+//
+//  Created by Samuel Seah on 21/4/15.
+//  Copyright (c) 2015 Samuel Seah. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
